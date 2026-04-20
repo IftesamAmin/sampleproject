f09e9b54738f1c5e43e9458dd291f21713f3237b
Iftesam Amin Bhuiyan <iftesam.itsd@goldenharvestbd.com>
2026-04-20 16:15:30 +0600

    added get api call

--- api.js
+++ api.js
@@ -7,3 +7,16 @@ export const dummyApiCall = async () => {
     }, 1000);
   });
 };
+
+// dummy get api call
+export const dummyGetApiCall = async () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        data: "dummy data",
+        status: 200,
+        message: "success",
+      });
+    }, 1000);
+  });
+};
